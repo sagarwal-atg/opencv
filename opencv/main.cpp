89b44f3de23045a09148f944ce37d68fad87a2b7
Shubhankar Agarwal <Somi@citeswpaurhprv-172-16-35-128.near.illinois.edu>
2015-08-28 23:28:13 -0500

    Initial Commit

---
+++ opencv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv
+//
+//  Created by Shubhankar Agarwal on 8/28/15.
+//  Copyright (c) 2015 Shubhankar Agarwal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
