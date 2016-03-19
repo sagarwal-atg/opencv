95ea64f7b3752ec2e354808970ef8ef3c857d336
shubhankar12 <shubhankar0109@gmail.com>
2016-03-18 23:33:12 -0500

    Baloon detected

--- opencv/main.cpp
+++ opencv/main.cpp
@@ -4,12 +4,30 @@
 //
 //  Created by Shubhankar Agarwal on 8/28/15.
 //  Copyright (c) 2015 Shubhankar Agarwal. All rights reserved.
-//
+//s
+    
+#include <cv.h>
+#include <highgui.h>
+    
+    using namespace cv;
+    
+    int main( int argc, char** argv )
+    {
+        Mat image;
+        image = imread( argv[1], 1 );
+        
+        if( argc != 2 || !image.data )
+        {
+            printf( "No image data \n" );
+            return -1;
+        }
+        
+        namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
+        imshow( "Display Image", image );
+        
+        waitKey(0);
+        
+        return 0;
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+
